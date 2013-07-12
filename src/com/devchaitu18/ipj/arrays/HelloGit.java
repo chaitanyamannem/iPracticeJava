0273f8285859033c935daa6b42e3c8b4e8135778
devchaitu18 <devchaitu18@gmail.com>
2013-07-12 18:32:07 +0530

    Intial Commit 2

---
+++ src/com/devchaitu18/ipj/arrays/HelloGit.java
@@ -0,0 +1,13 @@
+package com.devchaitu18.ipj.arrays;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloGit");
+
+	}
+
+}
