13f663897721afd9f16c43f8150deac4d2fb22a9
devchaitu18 <devchaitu18@gmail.com>
2013-07-13 01:54:33 +0530

    Intializing an array 2

--- src/com/devchaitu18/ipj/arrays/bsamples/IntializingAnArray.java
+++ src/com/devchaitu18/ipj/arrays/bsamples/IntializingAnArray.java
@@ -1,12 +1,31 @@
 package com.devchaitu18.ipj.arrays.bsamples;
 
+/**
+ * This is a basic sample class that demonstrates the intialization of an array.
+ * 
+ * @author devchaitu18
+ * 
+ */
 public class IntializingAnArray {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * When you declare an array you can specify the initial values of the
+		 * elements. This is done by providing a comma−delimited list between
+		 * braces [{ }] after an equal sign at the declaration point. The length
+		 * is automatically set based on the number of elements in the
+		 * comma-delimited list.
+		 */
+
+		// Intializing a primitive type array
+		int intArray[] = { 1, 2, 3 };
+		//This is also valid. The extra comma here is for the benefit of code genrators.
+		double doubleArray[] = {0.1,0.2,0.3,};
+
+		// TODO Intialzing a reference type array
 
 	}
 
