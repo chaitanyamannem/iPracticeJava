3814e37251aa0bca391562ba7fcf486a8c1267c2
devchaitu18 <devchaitu18@gmail.com>
2013-08-12 12:43:27 +0530

    Added Stub class for InvokingAllObjectClassMethodsOnArray

---
+++ src/com/devchaitu18/ipj/arrays/bsamples/InvokingAllObjectClassMethodsOnArray.java
@@ -0,0 +1,17 @@
+package com.devchaitu18.ipj.arrays.bsamples;
+
+/**
+ * This class demonstrates the effect or use of invoking all the methods of an
+ * Object class on a array.
+ * 
+ * @author devchaitu18
+ * 
+ */
+public class InvokingAllObjectClassMethodsOnArray {
+	// TODO InvokingAllObjectClassMethodsOnArray
+
+	public static void main(String[] args) {
+
+	}
+
+}
